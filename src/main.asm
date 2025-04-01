488c5f195f32241ba1972972820e05f04612acec
Sebastian Humm-Garø <sebastianhummgaro@icloud.com>
2025-04-01 14:37:33 +0200

    Moved input to its own file

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,6 @@
-include "include/hardware.inc"
+include "src/include/hardware.inc"
 include "src/utils/memcopy.asm"
+include "src/utils/input.asm"
 
 DEF BRICK_LEFT EQU $05
 DEF BRICK_RIGHT EQU $06
@@ -250,41 +251,7 @@ Right:
 	jp Main
 
 
-UpdateKeys:
-	;Poll half the controller
-	ld a, P1F_GET_BTN
-	call .onenibble
-	ld b, a ; B7 - 4 = 1 ;B3 - 0 = unpressed buttons
 
-	;poll the other half
-	ld a, P1F_GET_DPAD
-	call .onenibble
-	swap a ; A7-4 = unpressed directions ;A3-0 = 1
-	xor a, b ;A = pressed buttons + directions
-	ld b, a ; B pressed buttons + directions
-
-	;And release the controller
-	ld a, P1F_GET_NONE
-	ldh [rP1], a
-
-	;combine with previous wCurKeys to make wNewKeys
-	ld a, [wCurKeys]
-	xor a, b ; A = keys that changed state
-	and a, b ; A = keys that changed to pressed
-	ld [wNewKeys], a
-	ld a, b
-	ld [wCurKeys], a
-	ret
-
-.onenibble
-	ldh [rP1], a ; switch key matrix
-	call .knownret ; burn 10 cycles calling to a known ret
-	ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
-	ldh a, [rP1]
-	ldh a, [rP1] ; this read counts
-	or a, $F0 ;A7-4 = 1; A3-0 = unpressed keys
-.knownret
-	ret
 
 ; Check if a brick was collided with and breaks it if possible
 ; @param hl: address of tile
